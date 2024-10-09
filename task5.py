a8d11ecece353704b03cf815579bc1b2dd4f0514
Breandan Gillanders <b.gillanders1@universityofgalway.ie>
2024-10-09 15:31:10 +0100

    Reading and writing names from CSV

--- task5.py
+++ task5.py
@@ -1,2 +1,18 @@
 import csv
 
+with open("task5.csv", mode="w", newline='') as file:
+    writer = csv.writer(file)
+
+    while True:
+        name = input("Enter your name: ")
+        if name.lower() == "quit":
+            break
+
+        writer.writerow([name])
+
+with open("task5.csv", mode="r") as file:
+    reader = csv.reader(file)
+
+    print("Names in file:")
+    for row in reader:
+        print(row[0])
