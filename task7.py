6638e0884a367a84b49a9ebacb7a58ed328dbda2
Breandan Gillanders <b.gillanders1@universityofgalway.ie>
2024-10-09 15:42:49 +0100

    Sine and Cosine values saved

--- task7.py
+++ task7.py
@@ -3,3 +3,9 @@ import numpy as np
 
 x = np.linspace(0, 10, 101)
 print(x)
+
+cos_values = np.cos(x)
+sin_values = np.sin(x)
+
+np.save("task7_sin.npy", sin_values)
+np.save("task7_cos.npy", cos_values)
