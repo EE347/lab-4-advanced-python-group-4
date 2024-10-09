fd1738a7a7133dcf6037b0f1f7cc71a9a1c2ebb1
Breandan Gillanders <b.gillanders1@universityofgalway.ie>
2024-10-09 15:50:40 +0100

    Created plot

--- task8.py
+++ task8.py
@@ -1,2 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 101)
+cos = np.load("task7_cos.npy")
+sin = np.load("task7_sin.npy")
+
+plt.plot(x, sin, label="sin(x)")
+plt.plot(x, cos, label="cos(x)")
+plt.legend()
+
+plt.show()
